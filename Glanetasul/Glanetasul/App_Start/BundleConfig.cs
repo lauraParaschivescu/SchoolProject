0d52c3a3b47f81354d889aee6d66e6c6c3de575e
lauraparaschivescu <lauraparaschivescu1988@gmail.com>
2017-03-04 10:26:03 +0200

    Skeleton of the application - Empty page

---
+++ Glanetasul/Glanetasul/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Optimization;
+
+namespace Glanetasul
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/jquery-3.1.1.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/styles").Include(
+                      "~/Content/bootstrap.css"));
+
+#if DEBUG
+            BundleTable.EnableOptimizations = false;
+#else
+            BundleTable.EnableOptimizations = true;
+#endif
+        }
+    }
+}
